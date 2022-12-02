a7521852bb9da805dc90739f3416373e793053fe
John Blischak <jdblischak@gmail.com>
2022-12-02 13:45:09 -0500

    Clean up to remove libtiledb steps

--- scripts/nightly/checkout.sh
+++ scripts/nightly/checkout.sh
@@ -4,12 +4,8 @@ set -eux
 # Checkout Git repositories
 
 OWNER_TILEDB_MARIADB=${OWNER_TILEDB_MARIADB-TileDB-Inc}
-REF_LIBTILEDB=${REF_LIBTILEDB-dev}
 REF_MARIADB=${REF_MARIADB-mariadb-10.5.13}
 REF_TILEDB_MARIADB=${REF_TILEDB_MARIADB-HEAD}
-
-git clone https://github.com/TileDB-Inc/TileDB.git libtiledb
-git -C libtiledb checkout "${REF_LIBTILEDB}"
 
 git clone https://github.com/MariaDB/server.git mariadb
 git -C mariadb checkout "${REF_MARIADB}"
